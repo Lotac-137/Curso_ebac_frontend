b0686ca4e38c2506be01d7908e70901da66e9d37
José Marcelo <wikus.zms.ms@outlook.com>
2023-07-30 22:20:56 -0300

    Apagando comentarios

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-// Função para adicionar uma nova tarefa à lista
+
 function addTask(taskName) {
     const taskList = $('#taskList');
     const listItem = $('<li>').text(taskName);
     taskList.append(listItem);
 }
 
-  // Função para marcar uma tarefa como concluída
 function toggleTask(event) {
     $(event.target).toggleClass('completed');
 }
 
-  // Manipulador de evento para o formulário de adicionar tarefas
 $('#taskForm').submit(function(event) {
     event.preventDefault();
     const taskInput = $('#taskInput');
@@ -22,5 +20,4 @@ if (taskName !== '') {
     }
 });
 
-  // Manipulador de evento para marcar tarefa como concluída
 $('#taskList').on('click', 'li', toggleTask);
